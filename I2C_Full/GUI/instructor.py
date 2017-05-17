e4bdc070631c4c6bf50211c6fdb5b0dbe6a9307e
sotphium <giorgio@brown.edu>
2017-05-16 21:03:20 -0400

    Updated instructor GUI class

--- I2C_Full/GUI/instructor.py
+++ I2C_Full/GUI/instructor.py
@@ -1,9 +1,12 @@
 import time
+import smbus
+
 
 class instructor:
 
     def __init__(self):
         self.address = 0x04
+        self.bus = smbus.SMBus(1)
         #self.address1 = 0x04
         #self.address2 = 0x06
 
@@ -21,3 +24,7 @@ class instructor:
         else:
             outString = '0'+outString #signals that command is wrong (deal with this in Arduino code)
         print("Wrote: "+ outString)
+
+    	for character in outString: # convert into a string and iterate over it
+        	self.bus.write_byte(address, ord(character)) # send each char's ASCII encoding
+    	return -1
